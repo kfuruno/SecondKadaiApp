a65deaa5118d791fa2cc50b34100bda18f115253
kiyohiro.furuno <kfuruno.pm.et.conduct@gmail.com>
2021-08-11 22:40:13 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 古野清紘 on 2021/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
